9542e398d0fb777610e275ae4f711f456e9effd3
Ayesha Nizam <ayeshanizam39@gmail.com>
2025-07-23 18:12:47 +0500

    commit

--- src/pages/purchase.tsx
+++ src/pages/purchase.tsx
@@ -279,7 +279,7 @@ const purchase = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div><br/>
 
          <div className="flex flex-col justify-center">
           <div className="max-w-7xl w-full px-4 lg:px-4  text-justify">
