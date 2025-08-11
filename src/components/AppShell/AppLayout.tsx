ca552db682c3a75c0918e686455f3f4e849135c9
Ayesha Nizam <ayeshanizam39@gmail.com>
2025-08-11 18:08:57 +0500

    solved

--- src/components/AppShell/AppLayout.tsx
+++ src/components/AppShell/AppLayout.tsx
@@ -12,7 +12,7 @@ export function AppLayout({ children }: AppLayoutProps) {
     <div className="flex min-h-screen">
       <Sidebar />
 
-      <main className="lg:ml-60 ml-0 md:ml-0 dark:bg-black flex-1 items-center p-6 overflow-auto scrollbar-hidee bg-primary-bg">
+      <main className="lg:ml-60 ml-0 md:ml-0 dark:bg-black flex-1 items-center p-6 overflow-auto scrollbar-hide bg-primary-bg">
         <div className="flex justify-end ">
           <DarkModeToggle />
         </div>
